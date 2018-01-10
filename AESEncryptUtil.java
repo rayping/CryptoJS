6c8d5117e2f47a5a7aac8cc76cd501710338533d
leiyongping@hzyushi.cn <leiyongping@hzyushi.cn>
2018-01-10 15:13:10 +0800

    去掉关键key

--- AESEncryptUtil.java
+++ AESEncryptUtil.java
@@ -1,12 +1,3 @@
-/*
- * Copyright (C) 2016  HangZhou YuShi|ShaoXing XUN Hu Technology Co.Ltd  Holdings Ltd. All rights reserved
- *
- * 本代码版权归杭州宇石科技|绍兴迅虎所有，且受到相关的法律保护。
- * 没有经过版权所有者的书面同意，
- * 任何其他个人或组织均不得以任何形式将本文件或本文件的部分代码用于其他商业用途。
- *
- */
-package com.gws.utils;
 
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
